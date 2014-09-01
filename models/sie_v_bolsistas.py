1775afd91ffe227684667a9cd4093ff970da20cb
www-data <www-data@w2p.unirio.br>
2014-09-01 07:32:53 -0300

    Adicionados json e imagem para exibição no portal de sistemas

---
+++ models/sie_v_bolsistas.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+dbSie.define_table("V_BOLSISTAS_ATIVOS",
+                   Field("ID_CURSO_ALUNO", "integer"),
+                   Field("ID_PESSOA", "integer"),
+                   Field("NOME_PESSOA", "string"),
+                   Field("CPF_MASCARA", "string"),
+                   Field("CPF", "string"),
+                   Field("MATR_ALUNO", "string"),
+                   Field("NOME_CURSO_DIPLOMA", "string"),
+                   Field("ANO", "integer"),
+                   Field("COD_BOLSA", "string"),
+                   Field("ID_BOLSA", "integer"),
+                   primarykey=['ID_CURSO_ALUNO'],
+                   migrate=False
+                   )

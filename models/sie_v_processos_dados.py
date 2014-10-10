1211424bd65a3a839354f2a5fb01d3ee4e4ee81e
www-data <www-data@w2p.unirio.br>
2014-10-10 12:03:22 -0300

    Adidicionados modelos relacionados a views de PROCESSOS

---
+++ models/sie_v_processos_dados.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+dbSie.define_table("V_PROCESSOS_DADOS",
+                   Field("ID_DOCUMENTO", "integer"),
+                   Field("NUM_PROCESSO", "string"),
+                   Field("RESUMO_ASSUNTO", "string"),
+                   Field("EMINENTE", "string"),
+                   Field("DT_ALTERACAO", "date"),
+                   Field("HR_ALTERACAO", "time"),
+                   Field("DESCR_ASSUNTO", "string"),
+                   Field("COD_ESTRUTURADO", "string"),
+                   Field("NOME_INTERESSADO", "string"),
+                   Field("PROCEDENCIA", "string"),
+                   Field("NOME_TIPO_INTERESSADO", "string"),
+                   primarykey=['NUM_PROCESSO'],
+                   migrate=False,
+                   rname='DBSM.V_PROCESSOS_DADOS'
+                   )
---
+++ models/sie_v_processos_tramitacoes.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+dbSie.define_table("V_PROCESSOS_TRAMITACOES",
+                   Field("NUM_PROCESSO", "string"),
+                   Field("DT_ENVIO", "string"),
+                   Field("DT_RECEBIMENTO", "string"),
+                   Field("ID_ORIGEM", "integer"),
+                   Field("TIPO_ORIGEM", "integer"),
+                   Field("ID_DESTINO", "string"),
+                   Field("TIPO_DESTINO", "string"),
+                   Field("DESPACHO", "string"),
+                   Field("SITUACAO_TRAMIT", "string"),
+                   Field("RECEBIDO", "string"),
+                   Field("ORIGEM", "string"),
+                   Field("DESTINO", "string"),
+                   primarykey=['NUM_PROCESSO', 'DT_ENVIO', 'DT_RECEBIMENTO'],
+                   migrate=False,
+                   rname='DBSM.V_PROCESSOS_TRAMITACOES'
+                   )

7fe16986d7ba019f4bc35145c3829d052d8bac5d
diogomartins <diogo.martins@unirio.br>
2014-11-12 15:36:54 -0200

    Correção em V_PROJETOS_CLASSIFICACOES

--- models/sie_v_projetos_classificacoes.py
+++ models/sie_v_projetos_classificacoes.py
@@ -1,7 +1,8 @@
 dbSie.define_table("V_PROJETOS_CLASSIFICACOES",
+                   Field("ID_PROJETO"),
                    Field("CLASSIFICACAO", "string"),
                    Field("TIPO_CLASSIFICACAO", "string"),
-                   primarykey=["CLASSIFICACAO"],
+                   primarykey=["ID_PROJETO", "CLASSIFICACAO", "TIPO_CLASSIFICACAO"],
                    migrate=False,
                    rname='DBSM.V_PROJETOS_CLASSIFICACOES'
 )

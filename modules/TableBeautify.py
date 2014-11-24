085fccdc21442ee3b045acafdb696d2b16558abd
diogomartins <diogo.martins@unirio.br>
2014-11-24 16:37:23 -0200

    Ajustes de código estéticos em TableBeautify

--- modules/TableBeautify.py
+++ modules/TableBeautify.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from gluon import current
 from gluon.html import *
+
+
 class TableBeautify():
     excludedTables = ['COLUMNS']
 
@@ -10,21 +12,19 @@ class TableBeautify():
         self.descriptions = self._formatTableColumnsDescriptions(descriptions)
 
     def beautifyDatabaseTables(self):
-        tables = []
-        for tableName in self.tables:
-            if tableName not in self.excludedTables:
-                tables.append( self.printTable(tableName) )
-        return tables
+        return [self.printTable(tableName) for tableName in self.tables if tableName not in self.excludedTables]
 
     # Para uma determinada tabela, returna um TABLE Helper
     def printTable(self, tableName):
         table = TABLE(
-                      THEAD( TR(TH(tableName,_colspan=3)) ),
-                      THEAD( TR(TH('Field'), TH('Type'), TH('Description') ) ),
-                      TBODY( self._formatTableRows(tableName) ),
-                      _id='table_' + tableName,
-                      _class='tableDescriptions'
-                      )
+            THEAD(
+                TR(TH(tableName, _colspan=3)),
+                TR(TH('Field'), TH('Type'), TH('Description'))
+            ),
+            TBODY(self._formatTableRows(tableName)),
+            _id='table_' + tableName,
+            _class='tableDescriptions'
+        )
         return table
 
     # Para uma determinada tabela, retorna uma Lista de TR contendo duas colunas: "nome no campo" e "tipo de dado"
@@ -32,21 +32,21 @@ class TableBeautify():
         rows = []
         for field in self.dbSie[tableName].fields:
             fieldDataType = self.dbSie[tableName][field].type
-            rows.append( TR( TD(field),
-                             TD(fieldDataType, _class="row_type_" + fieldDataType),
-                             TD( self._getFieldDescription(tableName, field) )) )
+            rows.append(TR(TD(field),
+                           TD(fieldDataType, _class="row_type_" + fieldDataType),
+                           TD(self._getFieldDescription(tableName, field))))
         return rows
 
-    #===========================================================================
+    # ===========================================================================
     #
     #===========================================================================
     def _formatTableColumnsDescriptions(self, descriptions):
         tableColumnsDescriptions = {}
         for row in descriptions:
             if row.TABNAME in tableColumnsDescriptions:
-                tableColumnsDescriptions[row.TABNAME].update( {row.COLNAME : row.REMARKS} )
+                tableColumnsDescriptions[row.TABNAME].update({row.COLNAME: row.REMARKS})
             else:
-                tableColumnsDescriptions.update( {row.TABNAME : { row.COLNAME : row.REMARKS }} )
+                tableColumnsDescriptions.update({row.TABNAME: {row.COLNAME: row.REMARKS}})
 
         return tableColumnsDescriptions
 

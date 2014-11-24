f95a213722feb9e8afde6b54f228e991e1af7ec5
diogomartins <diogo.martins@unirio.br>
2014-11-24 10:39:49 -0200

    Teste de erro

--- models/sie_alunos.py
+++ models/sie_alunos.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-dbSie.define_table( "ALUNOS",
+dbSie.define_table("ALUNOS",
                     Field("ID_ALUNO", "integer"),
                     Field("ID_PESSOA", "integer"),
                     Field("ID_NATURALIDADE", "integer"),
-                    Field("SEXO", "integer"),
+                    Field("SEXO", "string"),
                     Field("DT_NASCIMENTO", "date"),
                     Field("ETNIA_ITEM", "integer"),
                     Field("ETNIA_TAB", "integer"),

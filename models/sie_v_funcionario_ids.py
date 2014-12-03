18be465aef95cad5e29f4ae0adfd802820c19fc8
diogomartins <diogo.martins@unirio.br>
2014-12-03 16:36:03 -0200

    Correção de typo em V_FUNCIONARIO_IDS

--- models/sie_v_funcionario_ids.py
+++ models/sie_v_funcionario_ids.py
@@ -1,11 +1,11 @@
 dbSie.define_table(
     "V_FUNCIONARIO_IDS",
-    Field("ID_USUARIO", "interger"),
-    Field("ID_PESSOA", "interger"),
-    Field("ID_FUNCIONARIO", "interger"),
-    Field("ID_CONTRATO_RH", "interger"),
+    Field("ID_USUARIO", "integer"),
+    Field("ID_PESSOA", "integer"),
+    Field("ID_FUNCIONARIO", "integer"),
+    Field("ID_CONTRATO_RH", "integer"),
     Field("CPF", "string"),
-    Field("CPF_MASCARA", "interger"),
+    Field("CPF_MASCARA", "string"),
     primarykey=["CPF"],
     migrate=False
 )

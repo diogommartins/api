2665665cfe7afbdfd61d8ca1c92d85b7721b014e
Leonardo Santos <leonardo.santos@unirio.br>
2014-12-17 12:44:15 -0200

    Renomeada a view V_DISCIPLINAS para V_CURSOS_DISCIPLINAS e adicinadas duas colunas, NIVEL_CURSO_ITEM e TIPO_CURSO_ITEM

--- models/sie_v_cursos_disciplinas.py
+++ models/sie_v_cursos_disciplinas.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
-dbSie.define_table("V_DISCIPLINAS",
+dbSie.define_table("V_CURSOS_DISCIPLINAS",
                    Field('NOME_CURSO', 'string'),
                    Field('NOME_CURSO_DIPLOMA', 'string'),
+                   Field('NIVEL_CURSO_ITEM', 'integer'),
+                   Field('TIPO_CURSO_ITEM', 'integer'),
                    Field('COD_ATIV_CURRIC', 'string'),
                    Field('NOME_ATIV_CURRIC', 'string'),
                    Field('OBRIGATORIA', 'string', length=1), #S-Sim, N-Nao
@@ -13,4 +15,4 @@ dbSie.define_table("V_DISCIPLINAS",
                    Field('ID_UNIDADE', 'integer'),
                    primarykey=['ID_DISC_CURRIC'],
                    migrate=False,
-                   rname="DBSM.V_DISCIPLINAS")
+                   rname="DBSM.V_CURSOS_DISCIPLINAS")

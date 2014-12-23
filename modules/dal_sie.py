0418d99b1096d27658ed4ef408237c7b67fd2479
diogomartins <diogo.martins@unirio.br>
2014-12-23 16:56:30 -0200

    Criado adaptador específico para lidar com BLOBS do SIE

--- modules/dal_sie.py
+++ modules/dal_sie.py
@@ -1,14 +1,8 @@
 import base64
 
-from gluon.dal import DAL, BaseAdapter
+from gluon.dal import DB2Adapter
 
 
-class DALSIE(DAL):
-    def __init__(self, **kwargs):
-        super(DALSIE, self).__init__(**kwargs)
-        self._adapter = SIEBaseAdapter(db=self, pool_size=0, uri='None', folder=self._folder, db_codec=self._db_codec)
-
-
-class SIEBaseAdapter(BaseAdapter):
+class SIEDB2BaseAdapter(DB2Adapter):
     def parse_blob(self, value, field_type):
         return base64.b64encode(str(value))

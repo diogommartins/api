c60468e78ee5133290aa0f5792f4a77c49522bda
diogomartins <diogo.martins@unirio.br>
2014-12-23 11:12:49 -0200

    Adicionado campo foto em ALUNOS

--- models/sie_alunos.py
+++ models/sie_alunos.py
@@ -7,6 +7,7 @@ dbSie.define_table("ALUNOS",
                     Field("DT_NASCIMENTO", "date"),
                     Field("ETNIA_ITEM", "integer"),
                     Field("ETNIA_TAB", "integer"),
+                    Field("FOTO", "blob"),
                     primarykey=["ID_ALUNO"],
                     migrate=False
                     )

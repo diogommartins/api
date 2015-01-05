f99fa722cfa963abd0971b5deed26c0469ad9857
diogomartins <diogo.martins@unirio.br>
2015-01-05 15:30:03 -0200

    Refatorações

--- models/1_dbconn.py
+++ models/1_dbconn.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 #Git ignored file
-from dal_sie import DALSIE
+from dal_sie import SIEDB2BaseAdapter
+from gluon.dal import ADAPTERS
 
-if request.is_local:
-    db = DAL('postgres://postgres:devdtic2@sistemas.unirio.br/api', migrate=False)
-    dbSie = db
-else:
-    db = DAL('postgres://postgres:devdtic2@sistemas.unirio.br/api')
-    dbSie = DALSIE('db2://DSN=dbsmtest;UID=dbsm;PWD=htrg11sn', db_codec='latin1')
+ADAPTERS.update({
+    'db2': SIEDB2BaseAdapter
+})
+
+db = DAL('postgres://postgres:devdtic2@sistemas.unirio.br/api')
+dbSie = DAL('db2://DSN=dbsmtest;UID=dbsm;PWD=htrg11sn', db_codec='latin1')
 

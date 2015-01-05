e3dfeb9c86cb289e335f783a1b173cf847a1ee31
diogomartins <diogo.martins@unirio.br>
2015-01-05 17:54:15 -0200

    Alteração no SIEDB2BaseAdapter para inserts de BLOBs

--- modules/dal_sie.py
+++ modules/dal_sie.py
@@ -6,3 +6,11 @@ from gluon.dal import DB2Adapter
 class SIEDB2BaseAdapter(DB2Adapter):
     def parse_blob(self, value, field_type):
         return base64.b64encode(str(value))
+
+    def represent_exceptions(self, obj, fieldtype):
+        if fieldtype == 'blob':
+            obj = base64.b64encode(str(obj))
+            return "BLOB('%s')" % base64.b64decode(obj)
+        elif fieldtype == 'datetime':
+            super(SIEDB2BaseAdapter, self).represent_exceptions(obj, fieldtype)
+        return None

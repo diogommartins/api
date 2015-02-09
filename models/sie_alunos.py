1d8701cbd641ebea6d1eae71736aec1cdbb409fb
diogomartins <diogo.martins@unirio.br>
2015-02-09 12:37:05 -0200

    Alterações nos modelos ALUNOS e V_PROJETOS_DADOS

--- models/sie_alunos.py
+++ models/sie_alunos.py
@@ -8,6 +8,9 @@ dbSie.define_table("ALUNOS",
                     Field("ETNIA_ITEM", "integer"),
                     Field("ETNIA_TAB", "integer"),
                     Field("FOTO", "blob"),
+                    Field("FOTO_WEB", "blob"),
+                    Field("NOME_PAI", "string"),
+                    Field("NOME_MAE", "string"),
                     primarykey=["ID_ALUNO"],
                     migrate=False
                     )

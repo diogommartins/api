cd6a0645f655681ff6a34d11e141efc9b3fa2067
diogomartins <diogo.martins@unirio.br>
2015-04-08 17:04:36 -0300

    Refatorações

--- modules/TableBeautify.py
+++ modules/TableBeautify.py
@@ -1,45 +1,49 @@
 # -*- coding: utf-8 -*-
-from gluon import current
 from gluon.html import *
 
 
 class TableBeautify(object):
     excludedTables = ['COLUMNS']
 
-    def __init__(self, tables, descriptions):
-        self.tables = tables
-        self.dbSie = current.dbSie
+    def __init__(self, db, descriptions):
+        self.db = db
         self.descriptions = self._formatTableColumnsDescriptions(descriptions)
 
     def beautifyDatabaseTables(self):
-        return [self.printTable(tableName) for tableName in self.tables if tableName not in self.excludedTables]
+        return [self.printTable(tableName) for tableName in self.db.tables if tableName not in self.excludedTables]
 
-    # Para uma determinada tabela, returna um TABLE Helper
-    def printTable(self, tableName):
+    def printTable(self, name):
+        """
+        Retorna uma tabela HTML correspondente a estrutura de um endpoint
+        :param name:
+        :rtype: gluon.html.TABLE
+        """
         table = TABLE(
             THEAD(
-                TR(TH(tableName, _colspan=3)),
+                TR(TH(name, _colspan=3)),
                 TR(TH('Field'), TH('Type'), TH('Description'))
             ),
-            TBODY(self._formatTableRows(tableName)),
-            _id='table_' + tableName,
+            TBODY(*self._formatTableRows(name)),
+            _id='table_' + name,
             _class='tableDescriptions'
         )
         return table
 
-    # Para uma determinada tabela, retorna uma Lista de TR contendo duas colunas: "nome no campo" e "tipo de dado"
-    def _formatTableRows(self, tableName):
-        rows = []
-        for field in self.dbSie[tableName].fields:
-            fieldDataType = self.dbSie[tableName][field].type
-            rows.append(TR(TD(field),
-                           TD(fieldDataType, _class="row_type_" + fieldDataType),
-                           TD(self._getFieldDescription(tableName, field))))
-        return rows
+    def _formatTableRows(self, name):
+        """
+        Para uma determinada tabela, retorna uma Lista de TR contendo duas colunas: "nome no campo" e "tipo de dado"
 
-    # ===========================================================================
-    #
-    #===========================================================================
+        :param name: O nome da tabela
+        :return:
+        """
+        for field in self.db[name].fields:
+            fieldDataType = self.db[name][field].type
+            yield TR(
+                TD(field),
+                TD(fieldDataType, _class="row_type_" + fieldDataType),
+                TD(self._getFieldDescription(name, field))
+            )
+
     def _formatTableColumnsDescriptions(self, descriptions):
         tableColumnsDescriptions = {}
         for row in descriptions:
@@ -50,8 +54,7 @@ class TableBeautify(object):
 
         return tableColumnsDescriptions
 
-    def _getFieldDescription(self, tableName, field):
-        if tableName in self.descriptions:
-            if field in self.descriptions[tableName]:
-                return self.descriptions[tableName][field]
+    def _getFieldDescription(self, name, field):
+        if name in self.descriptions and field in self.descriptions[name]:
+            return self.descriptions[name][field]
         return "Sem descrição"

16aaf7a5a7005fdb7c7f1e79ed92cb6ae14c283d
diogomartins <diogo.martins@unirio.br>
2015-05-05 17:07:20 -0300

    Refatoração.

--- modules/parsers.py
+++ modules/parsers.py
@@ -9,7 +9,9 @@ except ImportError:
 
 
 def json(value, default=custom_json):
-    """
-    replace JavaScript incompatible spacing http://timelessrepo.com/json-isnt-a-javascript-subset
-    """
-    return json_parser.dumps(value, default=default, ensure_ascii=False)
+    value = json_parser.dumps(value, default=default, ensure_ascii=False)
+    # replace JavaScript incompatible spacing
+    # http://timelessrepo.com/json-isnt-a-javascript-subset
+    value.replace(ur'\u2028', '\\u2028').replace(ur'\2029', '\\u2029')
+
+    return value

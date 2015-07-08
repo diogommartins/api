010a87a62d4f8b582e188d98140ec158d95b460f
diogomartins <diogo.martins@unirio.br>
2015-07-08 17:51:27 -0300

    Adicionada documentação da classe Procedure

--- modules/procedures/__init__.py
+++ modules/procedures/__init__.py
@@ -10,6 +10,12 @@ PROCEDURES = {
 
 class Procedure(object):
     def __call__(self, name):
+        """
+        Simple `Factory` class for BaseProcedure subclasses that returns a class reference
+
+        :param name: The name of a BaseProcedure subclass
+        :raise SyntaxError: Raised when trying to call a nonexistent procedure
+        """
         self.name = name
         if self.name not in PROCEDURES:
             raise SyntaxError("Procedure %s not supported." % self.name)

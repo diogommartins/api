977a97e38519bc9c6d487cfd43cb7fb434311e89
diogommartins <diogo.martins@unirio.br>
2015-08-20 15:33:04 -0300

    Refatoração em SIEDB2Adapter para que todas as operações enviem uma string utf-8 para o driver

--- modules/dal_sie.py
+++ modules/dal_sie.py
@@ -24,25 +24,11 @@ class SIEDB2BaseAdapter(DB2Adapter):
             super(SIEDB2BaseAdapter, self).represent_exceptions(obj, fieldtype)
         return None
 
-    def select(self, query, fields, attributes):
-        force_unicode = attributes.get('force_unicode', False)
-        if 'force_unicode' in attributes:
-            del attributes['force_unicode']
-        sql = self._select(query, fields, attributes)
+    def _select(self, query, fields, attributes):
+        return super(SIEDB2BaseAdapter, self)._select(query, fields, attributes).decode('utf-8')
 
-        if force_unicode:
-            sql = sql.decode('utf-8')
+    def _insert(self, table, fields):
+        return super(SIEDB2BaseAdapter, self)._insert(table, fields).decode('utf-8')
 
-        cache = attributes.get('cache', None)
-        if cache and attributes.get('cacheable',False):
-            del attributes['cache']
-            (cache_model, time_expire) = cache
-            key = self.uri + '/' + sql
-            key = hashlib_md5(key).hexdigest()
-            args = (sql,fields,attributes)
-            return cache_model(
-                key,
-                lambda self=self,args=args:self._select_aux(*args),
-                time_expire)
-        else:
-            return self._select_aux(sql,fields,attributes)
+    def _update(self, tablename, query, fields):
+        return super(SIEDB2BaseAdapter, self)._update(tablename, query, fields).decode('utf-8')

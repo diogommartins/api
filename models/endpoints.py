390ed94479c4245c3d62e78612e80a3acb4edd1f
diogommartins <diogo.martins@unirio.br>
2016-02-22 13:41:43 -0300

    Atualizada forma de definição on-demand de endpoints e seu respectivo load de models

--- models/endpoints.py
+++ models/endpoints.py
@@ -1,4 +1,5 @@
 # coding=utf-8
+from api.request import APIRequest
 from definers import Endpoints
 from modelers import JSONModelCreator, Web2pyModelCreator
 
@@ -9,11 +10,15 @@ endpoints_definer = Endpoints(datasource, schema='DBSM')
 def load_endpoints(write_models=False, refresh_cache=False):
     if write_models:
         endpoints_definer.add_observer(JSONModelCreator(request.folder + 'private/models.json'))
-        endpoints_definer.add_observer(Web2pyModelCreator(request.folder + 'models/'))
+        endpoints_definer.add_observer(Web2pyModelCreator(request.folder + 'models/endpoints/'))
 
     endpoints_definer.define_tables()
 
 
-if not request.controller == 'rest':
+if request.controller == 'rest':
+    endpoint = APIRequest.controller_for_rewrited_URL(request)
+    response.models_to_run += ['^endpoints/{endpoint}/\\w+\\.py$'.format(endpoint=endpoint)]
+    pass
+else:
     # if the request isn`t for an endpoint, load everything
     load_endpoints()

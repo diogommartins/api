5bed77a1d34021f4854729bffd03dd97ca93ca1d
diogommartins <diogo.martins@unirio.br>
2016-02-25 14:47:20 -0300

    Desabilitados workers. Instável.

--- models/procedures_workers.py
+++ models/procedures_workers.py
@@ -4,5 +4,5 @@ from procedures.worker import ProcedureWorker
 
 procedure_workers = [ProcedureWorker(db, datasource, procedure, ws_server) for procedure in PROCEDURES.keys()]
 
-for procedure in procedure_workers:
-    procedure.start()
+# for procedure in procedure_workers:
+#     procedure.start()
